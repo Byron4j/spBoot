3e2348a3c9a2ebd3fbe59f33f8eb641b4bc12c62
Byron4j <byron4j@126.com>
2018-10-14 11:53:24 +0800

    注解+XML

--- src/main/java/com/example/myboot/repository/UserRepository.java
+++ src/main/java/com/example/myboot/repository/UserRepository.java
@@ -13,21 +13,23 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserName(String userName);
     User findByUserNameOrEmail(String username, String email);
 
-    /*
+    /**
     自定义查询
     自定义的简单查询就是根据方法名来自动生成SQL，主要的语法是findXXBy,readAXXBy,queryXXBy,countXXBy, getXXBy后面跟属性名称：
      */
     List<User> findByNickName(String nickName);
-    // 不支持返回原始类型，返回int ---> error
+    /**
+     * 不支持返回原始类型，返回int ---> error
+      */
     Integer readByUserName(String userName);
 
-    /*
+    /**
     分页查询
      */
     List<User> findByUserName(String userName, Pageable pageable);
 
 
-    /*
+    /**
     限制插叙
      */
     User findTopByOrderByNickNameDesc();
@@ -35,7 +37,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findFirst10ByNickName(String nickName, Sort sort);
     List<User> findTop10ByNickName(String nickName, Pageable pageable);
 
-    /*
+    /**
     自定义SQL查询
      */
     @Query("select u from User u where u.id = ?1")
